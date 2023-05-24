4793d96d99a67ab094cde9d8c08bbad27fbe519c
Grace Nieboer <grace.nieboer@outlook.com>
2023-05-24 16:10:29 -0400

    iterative fibonacci translated to recursive fibonacci

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,8 @@
 function fibonacci(n) {
-  // type your code here
+  if (n < 2) {
+    return n
+  }
+  return fibonacci(n-1) + fibonacci(n-2)
 }
 
 if (require.main === module) {
@@ -8,6 +11,11 @@ if (require.main === module) {
   console.log("=>", fibonacci(0));
 
   console.log("");
+
+  console.log("Expecting: 1");
+  console.log("=>", fibonacci(1));
+
+  console.log("")
 
   console.log("Expecting: 1");
   console.log("=>", fibonacci(2));
@@ -22,3 +30,19 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// tke out the first two cases of 0, and 1.
+
+//solve without recursion:
+// function fibonacci(n) {
+//   if ( n < 2 ) {
+//     return n
+//   }
+//   else {
+//     let fib = [0, 1]
+//     for (let i = 2; i <= n; ++i) {
+//       fib.push(fib[i-1] + fib[i-2])
+//     }
+//   return fib[fib.length -1]
+//   }
+// }
